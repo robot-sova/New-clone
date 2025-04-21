ee64647bcb6d5341a1e40fd2b9d27005dd0f0117
robot-sova <6195738869abysov@gmail.com>
2025-04-21 09:58:33 -0700

    1

--- src/components/MegaMenu.jsx
+++ src/components/MegaMenu.jsx
@@ -75,13 +75,13 @@ export const MegaMenu = () => {
           {/* Home Dropdown */}
           <div className="dropdown">
            <button className="dropbtn" ><a href="/services/" onClick={() => handleDropdownToggle("Household")}>
-              Services <ArrowIcon isOpen={openDropdown === "Household"} />
+              Househols <ArrowIcon isOpen={openDropdown === "Household"} />
             </a></button>
             <div className={`dropdown-content ${openDropdown === "Household" ? "show" : ""}`}>
               <h3>High-End Appliances We Repair</h3>
               <div className="row">
                 {[
-                  [{label: "Refrigerator Repair", href: "/services/refrigerator-repair-los-angeles/" },
+                  [{label: "BBQ Grill Repar", href: "/bbq-grill-repair-los-angeles/" },
                     { label: "Oven Repair", href: "/services/oven-repair-los-angeles/" },
                   { label: "Stove Repair", href: "/stove-repair/" },
                   "Range Repair", "Cooktop Repair", "Range Hood Repair"],
@@ -109,6 +109,87 @@ export const MegaMenu = () => {
             </div>
           </div>
           {/* Commercial Dropdown */}
+          <div className="dropdown">
+           <button className="dropbtn" ><a href="/services/" onClick={() => handleDropdownToggle("Household")}>
+              Commercial <ArrowIcon isOpen={openDropdown === "Household"} />
+            </a></button>
+            <div className={`dropdown-content ${openDropdown === "Household" ? "show" : ""}`}>
+              <h3>High-End Appliances We Repair</h3>
+              <div className="row">
+                {[
+                  [{label: "Bar Refrigerator Repair", href: "/bar-refrigerator-repair-los-angeles/" },
+                    { label: "Commercial Dishwasher Repair", href: "/commercial-dishwasher-repair-los-angeles/" },
+                    { label: "Commercial Dryer Repair", href: "/commercial-dryer-repair-los-angeles/" },
+                  { label: "Commercial Freezer Repair", href: "/commercial-freezer-repair-los-angeles/" },
+                  { label: "Commercial Fryer Machine Repair", href: "/commercial-fryer-machine-repair-los-angeles/" },
+                  { label: "Commercial Ice Machine Repair", href: "/commercial-ice-machine-repair-los-angeles/" },
+                ],
+                  [{ label: "Commercial Laundry Machine Repair", href: "/commercial-laundry-machine-repair-los-angeles/" },
+                  { label: "Commercial Oven Repair", href: "/commercial-oven-repair-los-angeles/" },
+                  { label: "Commercial Refrigerator Repair", href: "/commercial-refrigerator-repair-los-angeles/" },
+                  { label: "Commercial Showcase Refrigerator Repair", href: "/commercial-showcase-refrigerator-repair-los-angeles/" },
+                  { label: "Commercial Slushie Machine Repair", href: "/commercial-slushie-machine-repair-los-angeles/" },],
+                  [ { label: "Commercial Stove Repair", href: "/commercial-stove-repair-los-angeles/" },
+                  { label: "Commercial Slushie Machine Repair", href: "/commercial-slushie-machine-repair-los-angeles/" },
+                  { label: "Commercial Walk-In Cooler Repair", href: "/commercial-walk-in-cooler-repair-los-angeles/" },
+                  { label: "Commercial Walk-In Freezer Repair", href: "/commercial-walk-in-freezer-repair-los-angeles/" },
+                  { label: "Commercial Washing Machine Repair", href: "/commercial-washing-machine-repair-los-angeles/" }, ],
+                ].map((col, idx) => (
+                  <div className="column" key={idx}>
+                    {col.map((item) =>
+                      typeof item === "string" ? (
+                        <a key={item} href="#">
+                          {/* <img src={`/svg/${item.split(" ")[0].toLowerCase()}.svg`} alt={item} /> */} {item}
+                        </a>
+                      ) : (
+                        <a key={item.label} href={item.href}>
+                          {/* <img src={`/svg/${item.label.split(" ")[0].toLowerCase()}.svg`} alt={item.label} /> */} {item.label}
+                        </a>
+                      )
+                    )}
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+          {/* HVAC Dropdown */}
+          <div className="dropdown">
+           <button className="dropbtn" ><a href="/services/" onClick={() => handleDropdownToggle("Household")}>
+              HVAC <ArrowIcon isOpen={openDropdown === "Household"} />
+            </a></button>
+            <div className={`dropdown-content ${openDropdown === "Household" ? "show" : ""}`}>
+              <h3>High-End Appliances We Repair</h3>
+              <div className="row">
+                {[
+                  [{label: "AC Repair", href: "/air-conditioner-repair-los-angeles/" },
+                    { label: "Air Duct Cleaning", href: "/air-ducts-cleaning-and-repair-los-angeles/" },
+
+                  { label: "Stove Repair", href: "/stove-repair/" },
+                  "Range Repair", "Cooktop Repair", "Range Hood Repair"],
+                  [{ label: "Wachine Machine Repair", href: "/washing-machine-repair/" },, "Dryer Repair", 
+                  { label: "Dishwasher Repair", href: "/dishwasher-repair/" },"Microwave Repair","Refrigerator Repair", "Freezer Repair",  ],
+                  ["Fireplace repair", "BBQ Repair", 
+                  { label: "Wine Cooler", href: "/wine-cooler-repair/" },
+                   "Wine Cellar Repair",  ],
+                ].map((col, idx) => (
+                  <div className="column" key={idx}>
+                    {col.map((item) =>
+                      typeof item === "string" ? (
+                        <a key={item} href="#">
+                          {/* <img src={`/svg/${item.split(" ")[0].toLowerCase()}.svg`} alt={item} /> */} {item}
+                        </a>
+                      ) : (
+                        <a key={item.label} href={item.href}>
+                          {/* <img src={`/svg/${item.label.split(" ")[0].toLowerCase()}.svg`} alt={item.label} /> */} {item.label}
+                        </a>
+                      )
+                    )}
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+          {/* Brands Dropdown */}
           <div className="dropdown">
           <button className="dropbtn"><a href="/brands/" onClick={() => handleDropdownToggle("commercial")}>
               Brands <ArrowIcon isOpen={openDropdown === "commercial"} />
@@ -176,54 +257,33 @@ export const MegaMenu = () => {
               </div>
             </div>
           </div>
+           {/* Online Estimates Dropdown */}
+           <div className="dropdown">
+            <button className="dropbtn" onClick={() => handleDropdownToggle("HVAC")}><a>
+              Estimates <ArrowIcon isOpen={openDropdown === "HVAC"} />
+            </a></button>
+            <div className={`dropdown-content ${openDropdown === "HVAC" ? "show" : ""}`}>
+              <div className="row">
+                {[
+                  ["Refrigerator Repair", "Oven Repair", "Dishwasher Repair", "Dryer Repair"],
+                  ["Washer Repair", "Microwave Repair", "Cooktop Repair", "Range Hood Repair"],
+                  ["Get a Quote", "Same Day Pricing", "Custom Requests"],
+                ].map((col, idx) => (
+                  <div className="column" key={idx}>
+                    {col.map((item) => (
+                       <a key={item} href="#">
+                    {/*   <img src={`/svg/${item.split(" ")[0].toLowerCase()}.svg`} alt={item} /> */}{item}
+                     </a>
+                    ))}
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
           
-             {/* Areas Dropdown */}
-             <div className="dropdown">
-            <button className="dropbtn" onClick={() => handleDropdownToggle("Areas")}>
-             <a> VIP<ArrowIcon isOpen={openDropdown === "Areas"} />
-            </a></button>
-            <div className={`dropdown-content ${openDropdown === "Areas" ? "show" : ""}`}>
-              <div className="row">
-                {[
-                  ["Yearly Maintenance"],
-                  ["Maintenance Twice A Year"],
-                  ["Donate To Low Income Familisies"],
-                ].map((col, idx) => (
-                  <div className="column" key={idx}>
-                    {col.map((item) => (
-                       <a key={item} href="#">
-                     {/*  <img src={`/svg/${item.split(" ")[0].toLowerCase()}.svg`} alt={item} /> */}{item}
-                     </a>
-                    ))}
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
+           
 
-          {/* Prices Dropdown */}
-          <div className="dropdown">
-            <button className="dropbtn" onClick={() => handleDropdownToggle("prices")}>
-           <a> Charity Program
-  <ArrowIcon isOpen={openDropdown === "prices"} />
-            </a></button>
-            <div className={`dropdown-content ${openDropdown === "prices" ? "show" : ""}`}>
-            <h3>10% of the revenues goes towards helping law income families fix their appliances!</h3>
-              <div className="row">
-                {[
-                  [],
-                ].map((col, idx) => (
-                  <div className="column" key={idx}>
-                    {col.map((item) => (
-                       <a key={item} href="#">
-                      {/* <img src={`/svg/${item.split(" ")[0].toLowerCase()}.svg`} alt={item} />*/} {item}
-                     </a>
-                    ))}
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
+       
           {/* Phone Number */}
           <a href="#top" className="phone-number">(213) 309-1924</a>
         </div>
